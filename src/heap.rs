fa9c12b90aaf0afca0c3f843babdfe47df3cd8ad
pravin <pravinramana25@protonmail.ch>
2021-11-04 17:42:47 -0500

    Implement insert function

--- src/heap.rs
+++ src/heap.rs
@@ -1,8 +1,13 @@
+// Implemented as a MaxHeap
+#[derive(Debug)]
 pub struct Heap<T> {
     heap: Vec<T>,
 }
 
 impl<T: std::cmp::PartialOrd> Heap<T> {
+    pub fn new() -> Self {
+        Self { heap: Vec::new() }
+    }
     pub fn peek(&self) -> Option<&T> {
         self.heap.first()
     }
@@ -15,8 +20,20 @@ impl<T: std::cmp::PartialOrd> Heap<T> {
     fn parent(index: usize) -> usize {
         index / 2
     }
-    pub fn insert(&mut self, value: T) {}
-    fn bubble_up(&mut self, index: usize) {}
+    pub fn insert(&mut self, value: T) {
+        self.heap.push(value);
+        self.bubble_up(self.heap.len() - 1)
+    }
+    fn bubble_up(&mut self, index: usize) {
+        if index == 0 {
+            return;
+        }
+        let parent_index = Self::parent(index);
+        if self.heap[index] > self.heap[parent_index] {
+            self.heap.swap(index, parent_index);
+            self.bubble_up(parent_index);
+        }
+    }
     fn pop(&mut self, index: usize) {}
     fn sift_down(&mut self, index: usize) {}
 }
